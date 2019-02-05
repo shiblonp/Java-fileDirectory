3e6592a906853c873e42d8da6247bdae2f20a5e4
Shiblon <spahulu@byu.edu>
2019-02-04 19:24:15 -0600

    added functions to write to filesystem straight from the program

--- src/com/pluralsight/filesystem/Main.java
+++ src/com/pluralsight/filesystem/Main.java
@@ -1,16 +1,15 @@
 package com.pluralsight.filesystem;
 import static java.nio.file.StandardCopyOption.*;
+
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.nio.file.Files;
-
 
 
 //Created on 2/4 to demonstrate how Java supports FileSystems
@@ -30,6 +29,8 @@ public class Main {
 
         try(FileSystem zipFs = openZip(Paths.get("myData.zip"))){
             copyToZip(zipFs);
+            writeToFileInZip1(zipFs,data);
+            writeToFileInZip2(zipFs,data);
         }catch(Exception e){
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
@@ -49,10 +50,27 @@ public class Main {
 
         return zipFs;
     }
+    //reads a default file and copies it into a zip file system
     private static void copyToZip(FileSystem zipFs) throws IOException{
         Path sourceFile = Paths.get("file1.txt");
         Path destFile = zipFs.getPath("/file1Copied.txt");
         Files.copy(sourceFile,destFile, REPLACE_EXISTING);
+    }
+    //Methods to show that I can write to the filesystem directly from the program
+    private static void writeToFileInZip1(FileSystem zipFs, String [] data) throws IOException
+    {
+        try(BufferedWriter writer = Files.newBufferedWriter(zipFs.getPath("/newFile1.txt")))
+        {
+            for(String d:data){
+                writer.write(d);
+                writer.newLine();
+            }
+        }
+    }
+    private static void writeToFileInZip2(FileSystem zipFs, String [] data)throws IOException
+    {
+        //FileSystems way of writing the file, another way to do what is written in the previous method
+    Files.write(zipFs.getPath("/newFile2.txt"), Arrays.asList(data), Charset.defaultCharset(), StandardOpenOption.CREATE);
     }
 }
 
